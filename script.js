3c8886fa58d38312978ff3d924b14f4feb536371
sadore-iq <ali0mzaffar@gmail.com>
2026-05-31 18:26:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ const PRODUCTS = [
   { id: 1,  name: 'سيروم امبولات من سادور حمض الهيالورونيك',     price: 5500, image: 'https://github.com/sadore-iq/sadore/raw/refs/heads/main/%D8%B3%D9%8A%D8%B1%D9%88%D9%85%20%D8%A7%D9%85%D8%A8%D9%88%D9%84%D8%A7%D8%AA%20%D9%85%D9%86%20%D8%B3%D8%A7%D8%AF%D9%88%D8%B1%20%D8%AD%D9%85%D8%B6%20%D8%A7%D9%84%D9%87%D9%8A%D8%A7%D9%84%D9%88%D8%B1%D9%88%D9%86%D9%8A%D9%83.avif' },
   { id: 2,  name: ' غسول كافيار بحمظ الهايلورنك ',           price: 10000, image: 'https://raw.githubusercontent.com/sadore-iq/sadore/refs/heads/main/IMG-20260531-WA0109.jpg' },
   { id: 3,  name: ' سيروم الكولاجين من سادور ',             price: 10000, image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0010.jpg?raw=true' },
-  { id: 4,  name: 'ايكتون من شركة سادور',       price: 95,  image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0107.jpg?raw=true' },
-  { id: 5,  name: 'سيروم الأفوكادو',       price: 380, image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0110.jpg?raw=true' },
-  { id: 6,  name: '  نياسيناميد & فيتامينC',            price: 260, image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0111.jpg?raw=true' },
+  { id: 4,  name: 'ايكتون من شركة سادور',       price: 6000,  image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0107.jpg?raw=true' },
+  { id: 5,  name: 'سيروم الأفوكادو',       price: 4500, image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0110.jpg?raw=true' },
+  { id: 6,  name: '  نياسيناميد & فيتامينC',            price: 3500, image: 'https://github.com/sadore-iq/sadore/blob/main/IMG-20260531-WA0111.jpg?raw=true' },
 
   // ── المجموعة الثانية ──
   { id: 7,  name: 'محفظة جلدية رجالية',          price: 85,  image: 'https://images.unsplash.com/photo-1627123424574-724758594e93?w=400&q=80' },
